ee036d9aa534ee4b63e1a8d1a1d77e59937d49d9
MatthieuTrr <terrier.matthieu.41@gmail.com>
2025-11-15 22:33:14 +0100

    Update trid_per_C2D.m

--- trid_per_C2D.m
+++ trid_per_C2D.m
@@ -1,3 +1,9 @@
+% ===============================================
+% fonction utilisant la transformation de matrices
+% périodiques et l'algorithme de Thomas pour ré-
+% -soudre m systèmes linéaires à matrices périodi-
+% -ques.
+% ===============================================
 function [fi]=trid_per_C2D(aa,ab,ac,fi)
 
 end
