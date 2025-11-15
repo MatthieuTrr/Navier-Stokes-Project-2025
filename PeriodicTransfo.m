b2037b84804e9a7e1c853d5f7edb9c1b615770eb
MatthieuTrr <terrier.matthieu.41@gmail.com>
2025-11-15 22:27:41 +0100

    Update PeriodicTransfo.m

--- PeriodicTransfo.m
+++ PeriodicTransfo.m
@@ -1,5 +1,11 @@
+% ===============================================
+% Algorithme permettant la transformation d'un
+% système linéaire à matrice périodique en un
+% système à matrice tridiagonale.
+% ===============================================
 function [X] = PeriodicTransfo(Per, f)
 
 
 
 end
+
