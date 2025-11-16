212adb03d093153bf4f651847f2b25103bfc65a6
MatthieuTrr <terrier.matthieu.41@gmail.com>
2025-11-16 11:03:52 +0100

    Update trid_per_C2D.m

--- trid_per_C2D.m
+++ trid_per_C2D.m
@@ -5,5 +5,10 @@
 % -ques.
 % ===============================================
 function [fi]=trid_per_C2D(aa,ab,ac,fi)
-
+ % On souhaite résoudre m systèmes linéaires périodiques.
+ % L'idée est de "stocker" nos systèmes tridiagonaux dans
+ % 3 matrices: aa, ab, ac; contenant respectivement les 3
+ % diagonales de tous les systèmes en colonnes. aa est donc
+ % une matrice de taille n*m dont les colonnes sont les sous-
+ % diagonales de chaque système.
 end
