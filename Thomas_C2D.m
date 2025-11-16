a6817c8f7f830f30383d4549aa3a409137015b52
MatthieuTrr <terrier.matthieu.41@gmail.com>
2025-11-16 11:18:48 +0100

    Update Thomas_C2D.m

--- Thomas_C2D.m
+++ Thomas_C2D.m
@@ -3,15 +3,23 @@
 % -naux utilisant le calcul vectoriel.
 % ===============================================
 function [X] = Thomas_C2D(aa,ab,ac,fi)
+% On utilise le calcul vectoriel pour réaliser
+% exactement les mêmes calculs que dans l'algo-
+% -rithme de Thomas pour m sytèmes en même temps.
   [m,n]=size(fi);
   beta = zeros(m, n);
   gamma = zeros(m, n);
   x = zeros(m, n);
-  beta(:,1)=aa(:,1);
+  beta(:,1)=ab(:,1);
   gamma(:,1)=fi(:,1)/ab(:,1);
-
+  for i=2:n
+    beta(:,i)=ab(:,i)-ac(:,i-1)./beta(:,i-1).*aa(:,i);
+    gamma(:;i)=(fi(:,i)-aa(:;i).*gamma(:,i-1))./beta(:;i);
+  end
+  
 
 end
+
 
 
 
