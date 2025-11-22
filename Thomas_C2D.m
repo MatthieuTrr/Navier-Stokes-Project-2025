386e0bdcc158151c483fabc1d37c88310d93b72d
MatthieuTrr <terrier.matthieu.41@gmail.com>
2025-11-22 12:26:42 +0100

    Update Thomas_C2D.m

--- Thomas_C2D.m
+++ Thomas_C2D.m
@@ -6,25 +6,24 @@ function [x] = Thomas_C2D(aa,ab,ac,fi)
 % On utilise le calcul vectoriel pour réaliser
 % exactement les mêmes calculs que dans l'algo-
 % -rithme de Thomas pour m sytèmes en même temps.
-  [m,n]=size(fi);
-  beta = zeros(m, n);
-  gamma = zeros(m, n);
-  x = zeros(m, n);
-  beta(:,1)=ab(:,1);
-  gamma(:,1)=fi(:,1)./ab(:,1);
+  [n,m]=size(fi);
+  beta = zeros(n,m);
+  gamma = zeros(n,m);
+  x = zeros(n,m);
+  beta(1,:)=ab(1,:);
+  gamma(1,:)=fi(1,:)./ab(1,:);
   for i=2:n
-    beta(:,i)=ab(:,i)-ac(:,i-1)./beta(:,i-1).*aa(:,i);
-    gamma(:,i)=(fi(:,i)-aa(:,i).*gamma(:,i-1))./beta(:,i);
+    beta(i,:)=ab(i,:)-ac(i-1,:).*aa(i-1,:)./beta(i-1,:);
+    gamma(i,:)=(fi(i,:)-aa(i-1,:).*gamma(i-1,:))./beta(i,:);
   end
-  x(:,n)=gamma(:,n);
+  x(n,:)=gamma(n,:);
   for j=n-1:-1:1
-      x(:,j) = gamma(:,j) - ac(:,j) ./ beta(:,j) .* x(:,j+1);
+      x(j,:) = gamma(j,:) - ac(j,:).* x(j+1,:) ./ beta(j,:) ;
   end
 
 
 
 end
-
 
 
 
